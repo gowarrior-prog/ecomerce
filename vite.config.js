50a6c49d0d41a700c12920397292557f1afc12ba
gowarrior-prog <goworriess@gmail.com>
2026-03-03 15:24:54 +0500

    mojmvfvf

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  basename: '/ecomerce/'
+  base: '/ecomerce/'   // ← 'basename' → 'base' (correct property name)
 })
